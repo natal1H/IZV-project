471fb49fa3ba5c820ca432641a82482a4bb7f23b
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2020-11-02 22:56:27 +0100

    Fixed graph size

--- get_stat.py
+++ get_stat.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from download import DataDownloader
 
 
@@ -6,7 +7,7 @@ def plot_stat(data_source, fig_location=None, show_figure=False):
     # TODO - check data_source if valid (not None)
 
     # Create dictionary region name - number of accidents
-    accidents = {"2016": None, "2017": None, "2018": None, "2019": None, "2020": None} # TODO: nech si automaticky zisti ake roky tam su
+    accidents = {"2016": None, "2017": None, "2018": None, "2019": None, "2020": None}  # TODO: nech si automaticky zisti ake roky tam su
 
     # Iterate over all accident records
     for i in range(data_source[1][0].size):
@@ -23,10 +24,14 @@ def plot_stat(data_source, fig_location=None, show_figure=False):
         else:
             accidents[year][region] += 1
 
-    # TODO: find max number of accident to set y limit on graph (slightly bigger than it)
+    # find max number of accident to set y limit on graph (slightly bigger than it)
+    accidents_by_years = [[num for region, num in stats.items()] for stats in accidents.values()]
+    all_accidents = [inner for outer in accidents_by_years for inner in outer]  # join all nested lists into one list
+    max_accidents = max(all_accidents)
 
     # prepare figure
-    plt.figure(figsize=(10, 10)) # TODO - nastaviť správne rozmery
+    dpi = 96  # typical monitor dpi in order to get size in pixels we want
+    plt.figure(figsize=(1000/dpi, 1600/dpi), dpi=dpi)
     plt.suptitle("Počty nehôd v krajoch Českej republiky")
     num_rows = len(accidents.keys())
     plot_index = 1
@@ -47,21 +52,20 @@ def plot_stat(data_source, fig_location=None, show_figure=False):
         plt.title(year)  # Subplot title
         plt.grid(axis="y", zorder=0)  # Horizontal grid
         plt.ylabel("Počet nehôd")  # Y label
+        plt.ylim(top=max_accidents + 4000)
+        plt.yticks(np.arange(0, max_accidents + 4000, 4000))
         plt.bar(regions, num_accidents, zorder=3)  # zorder has to be 3 so that grid is behind bars
         xlocs, xlabs = plt.xticks()
-        # plt.tight_layout(pad=2.0)
         plt.subplots_adjust(hspace=0.8)
 
-        for i, v in enumerate(num_accidents):  # TODO - will display at bottom of bar, change it to below top
-            #plt.text(i - .25, v / num_accidents[i] + 100, num_accidents[i])
-            #plt.text(i - 0.3, num_accidents[i] - 2000, num_accidents[i], fontsize=8)
+        for i, v in enumerate(num_accidents):  # show exact number of accidents
             plt.text(xlocs[i] - 0.3, num_accidents[i] - 2000, num_accidents[i], fontsize=8)
 
-        for i, v in enumerate(order):
+        for i, v in enumerate(order):  # show region order in list of accidents sorted from max to min
             plt.text(xlocs[i] - 0.1, num_accidents[i] + 500, str(v))
 
         plot_index += 1
-    plt.show()
+    plt.savefig('my_fig.png', dpi=dpi)
     plt.close()
 
 
