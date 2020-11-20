8ce17cf0e7515c5966275af12661df53da994e94
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2020-11-20 16:02:03 +0100

    Added empty framework for 2nd part of project

---
+++ analysis.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3.8
+# coding=utf-8
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import os
+# muzete pridat libovolnou zakladni knihovnu ci knihovnu predstavenou na prednaskach
+# dalsi knihovny pak na dotaz
+
+# Ukol 1: nacteni dat
+def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
+    pass
+
+# Ukol 2: následky nehod v jednotlivých regionech
+def plot_conseq(df: pd.DataFrame, fig_location: str = None,
+                show_figure: bool = False):
+    pass
+
+# Ukol3: příčina nehody a škoda
+def plot_damage(df: pd.DataFrame, fig_location: str = None,
+                show_figure: bool = False):
+    pass
+
+# Ukol 4: povrch vozovky
+def plot_surface(df: pd.DataFrame, fig_location: str = None,
+                 show_figure: bool = False):
+    pass
+
+
+if __name__ == "__main__":
+    pass
+    # zde je ukazka pouziti, tuto cast muzete modifikovat podle libosti
+    # skript nebude pri testovani pousten primo, ale budou volany konkreni ¨
+    # funkce.
+    df = get_dataframe("accidents.pkl.gz")
+    plot_conseq(df, fig_location="01_nasledky.png", show_figure=True)
+    plot_damage(df, "02_priciny.png", True)
+    plot_surface(df, "03_stav.png", True)
