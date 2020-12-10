4baa79d7dc52ac8a6822aa1fcbc785d76619e5e6
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2020-12-10 21:33:51 +0100

    Task 1: make_geo done

--- geo.py
+++ geo.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3.8
 # coding=utf-8
+
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
 import contextily
 import sklearn.cluster
 import numpy as np
-# muzeze pridat vlastni knihovny
 
+"""IZV project - Final part
+    Author: Natália Holková
+    Login: xholko02
+"""
+
+REGION = 'JHM'
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
-    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
+    """
+    Converts dataframe to geopandas.GeoDataFrame with correct encoding
+    :param df: DataFrame containing accidents data
+    :return: geopandas.GeoDataFrame
+    """
+    if df is not None:
+        # convert DataFrame to geopandas.GeoDataFrame
+        gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514")
+        gdf = gdf[gdf["d"].notna()]  # drop rows where "d" is nan
+        gdf = gdf[gdf["e"].notna()]  # drop rows where "e" is nan
+        return gdf
+    else:  # df is None
+        raise ValueError("No DataFrame provided.")
 
 
-def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
-             show_figure: bool = False):
+def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None, show_figure: bool = False):
     """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
 
 
-def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
-                 show_figure: bool = False):
+def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None, show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
 
 
